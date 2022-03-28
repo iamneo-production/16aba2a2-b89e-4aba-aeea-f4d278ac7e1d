14f9a84d094de31a8e46c72005122119241c36f5
RanjanKumar666 <typhonazazel@gmail.com>
2022-03-28 21:26:54 +0530

    testing 3 cors fix

--- springapp/src/main/java/com/examly/springapp/model/Like.java
+++ springapp/src/main/java/com/examly/springapp/model/Like.java
@@ -8,11 +8,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.GenericGenerator;
 
